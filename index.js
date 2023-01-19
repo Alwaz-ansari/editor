cb69ff3e862d46bb4c51681d0bc26d09eac224a5
Alwaz-ansari <118388500+Alwaz-ansari@users.noreply.github.com>
2023-01-19 20:39:50 +0530

    commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Js codes go here
+ 
+
+ function run() {
+     let html = document.getElementById('html').value;
+     let css = document.getElementById('css').value;
+     let js = document.getElementById('js').value;
+     let output = document.getElementById('output');
+     output.contentDocument.body.innerHTML = "<html>"+html+"</html>" + "<style>" + css + "</style>";
+     output.contentWindow.eval(js);
+ }
